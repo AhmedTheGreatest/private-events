2067e610cdf678b28081e03bf7931e3f20ceeb66
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-06-07 22:23:57 +0500

    Improve UI by adding CSS and refactoring HTML

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,6 +14,7 @@ class Event < ApplicationRecord
     left_joins(:attendees)
       .where(events: { creator_id: user.id })
       .or(where(users: { id: user.id }))
+      .distinct
   end
 
   def accessible_by?(user)
